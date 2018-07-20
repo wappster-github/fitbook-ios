84b8591c2c786e1501a3ab54fb9dcc57a6af0802
haroun.smida <smida.haroun@gmail.com>
2018-07-20 17:56:04 +0100

    Initial commit

---
+++ fitbook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fitbook
+//
+//  Created by ODC on 7/20/18.
+//  Copyright © 2018 Orange Developer Center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
